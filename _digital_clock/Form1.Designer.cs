d49f3a6918174185a1aff177832e9b97543fcb4a
Arslan Tariq <arslantariq4546@gmail.com>
2024-03-19 11:43:28 +0500

    initial commit

---
+++ _digital_clock/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace _digital_clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ControlDarkDark;
+            label1.Font = new Font("Segoe UI Black", 36F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.Info;
+            label1.Location = new Point(382, 258);
+            label1.Name = "label1";
+            label1.Size = new Size(344, 96);
+            label1.TabIndex = 0;
+            label1.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(818, 147);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 25);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(240, 160);
+            label3.Name = "label3";
+            label3.Size = new Size(59, 25);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = SystemColors.ControlDarkDark;
+            label4.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.Info;
+            label4.Location = new Point(349, 28);
+            label4.Name = "label4";
+            label4.Size = new Size(419, 96);
+            label4.TabIndex = 3;
+            label4.Text = "wednesday";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = SystemColors.ControlDarkDark;
+            label5.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.Info;
+            label5.Location = new Point(340, 147);
+            label5.Name = "label5";
+            label5.Size = new Size(428, 96);
+            label5.TabIndex = 4;
+            label5.Text = "20_02_2024";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1042, 442);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "digital_clock";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private FolderBrowserDialog folderBrowserDialog1;
+    }
+}
